621c899c209f737bd7cafc79b5f4912ec211d839
Keb Cuevas <51538653+keb-cuevas@users.noreply.github.com>
2022-04-15 15:45:46 +0800

    Reverted back

--- app.py
+++ app.py
@@ -3,6 +3,23 @@ from flask import Flask, render_template
 from flask import request
 import pandas as pd
 
+path = 'seeds_dataset.txt'
+
+df = pd.read_csv(path, sep= '\t', header= None,
+                names=['area','perimeter','compactness','lengthOfKernel',
+                       'widthOfKernel','asymmetryCoefficient',
+                      'lengthOfKernelGroove','seedType'])
+
+df = df.dropna()
+df.info()
+
+X= df.drop('seedType', axis = 1)
+y= df['seedType']
+
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.30, random_state=42)
+
 #do not show warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -17,6 +34,28 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
+
+#create an array of models
+models = []
+models.append(("LR",LogisticRegression()))
+models.append(("NB",GaussianNB()))
+models.append(("RF",RandomForestClassifier()))
+models.append(("SVC",SVC()))
+models.append(("Dtree",DecisionTreeClassifier()))
+models.append(("XGB",xgb.XGBClassifier()))
+models.append(("KNN",KNeighborsClassifier()))
+
+#measure the accuracy 
+for name,model in models:
+    kfold = KFold(n_splits=2)
+    cv_result = cross_val_score(model,X_train,y_train, cv = kfold,scoring = "accuracy")
+    print(name, cv_result)
+
+    xgb_model = xgb.XGBClassifier().fit(X_train, y_train)
+
+import joblib
+
+joblib.dump(xgb_model, "xgb.pkl") #export ML model to pkl file
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -62,5 +101,5 @@ def text():
   return render_template('text.html', output = prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-	#app.run
+    app.run(host='0.0.0.0', port=8000, debug=True)
+	#app.run(debug=True)
